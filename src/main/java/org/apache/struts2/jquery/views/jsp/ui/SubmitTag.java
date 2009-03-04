ab8ce61e5f0ff1532b58c48203fc661c74995002
obinna <obinna@b6f35aee-01e9-11de-a295-0b7b0935bf94>
2009-03-04 20:29:38 +0000

    - Refactored tag/component structure - Added showcase app skeleton

---
+++ src/main/java/org/apache/struts2/jquery/views/jsp/ui/SubmitTag.java
@@ -0,0 +1,32 @@
+package org.apache.struts2.jquery.views.jsp.ui;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.components.Component;
+import org.apache.struts2.jquery.components.Submit;
+
+import com.opensymphony.xwork2.util.ValueStack;
+
+public class SubmitTag extends AbstractActionTag {
+	
+	private static final long serialVersionUID = -3598941881606117018L;
+			
+	public SubmitTag() {
+		super();
+	}
+
+	@Override
+	public Component getBean(ValueStack stack, HttpServletRequest req, HttpServletResponse res) {
+		Component bean = new Submit(stack, req, res);
+		return bean;
+	}
+
+	@Override
+    protected void populateParams() {
+        
+		super.populateParams();
+
+       //Anchor anchor = (Anchor) component;
+    }
+}
